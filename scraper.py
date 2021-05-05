4e6f0e7df9f774f325bcacf963a1afa114821533
Joya <monishij@uci.edu>
2021-05-04 18:38:25 -0700

    added notes for interview

--- scraper.py
+++ scraper.py
@@ -1,10 +1,15 @@
 import re
 import nltk
+# RegexpTokenizer for splitting tokenizer based off specd regexp pattern
 from nltk.tokenize import RegexpTokenizer
+# parses url text into a useable URL object
 from urllib.parse import urlparse
+# ConfigParser reads info from config file
 from configparser import ConfigParser
+# our lovely webscraper that creates an object based on HTML text
 from bs4 import BeautifulSoup
 from collections import defaultdict, OrderedDict
+# downloads a dependency thats needed for nltk to run
 nltk.download('punkt')
 
 '''
@@ -52,11 +57,11 @@ GLOBAL VARIABLES TO ANSWER QUESTIONS
 unique_urls = set()
 # subdomains will contain key: subdomain, value: set(unique_urls) to answer question 4
 subdomains = defaultdict(set)
-#url and word count of webpage with the most words
+# url and word count of webpage with the most words -> with placeholders
 longest_page = ("www", float("-inf"))
 # dictionary to count word frequency
 word_count = defaultdict(int)
-# put in all words not in stop word
+# read stopword.txt into a set
 stop_words = set()
 with open('stop_words.txt') as f:
     for line in f:
@@ -67,28 +72,34 @@ with open('stop_words.txt') as f:
 # reading all acceptable domains from config file
 config = ConfigParser()
 config.read('config.ini')
+# gets the domain part of the url
 valid_domains = config['CRAWLER']['SEEDURL'].split(',')
 
 def scraper(url, resp):
+    # lets the function use global variable (python complained when this wasnt here)
     global longest_page
     # resp.raw_response.content gives HTML content, which we can pass to BeautifulSoup(content, 'lxml')
     # then to get all text on the page, use soup.get_text() -> answer the different qs/do stuff with it
     # then call extract_next_links() to get all links on this page -> we can validate the links with is_valid()
 
     # dictionary to keep track of output file writes, we will use subdomains dict for #4 output and longest_page tuple for #2 output
+    # ordered dict preserves order of insertion
     output = OrderedDict()
     output['WEBPAGE AND RESPONSE STATUS'] = f'{url} ---- {resp.status}'
 
+    # get rid of parts of URL that are irrelevant or could lead to traps
     url_no_fragment = sanitize_url(url)
 
+    # status 200 means we retrieved webpage succesfully and we haven't crawled it yet
     if resp.status == 200 and url_no_fragment not in unique_urls:
-        # received webpage -> convert to beautifulsoup object
+        # received webpage -> convert to beautifulsoup object containing just the content, lxml because it was the recommended parser
         soup = BeautifulSoup(resp.raw_response.content, 'lxml')
 
-        # Avoid webpages without less than one div because these commonly do not hold much valuable information
+        # LOW INFO VALUE: Avoid webpages without less than one div because these commonly do not hold much valuable information
         if len(soup.find_all("div")) < 1:
             return []
-
+        
+        # retrieve all of the text from the webpage
         text = soup.get_text()
         
         # QUESTION 1 CODE: substring url to discard fragment and add to unique_urls
@@ -97,39 +108,61 @@ def scraper(url, resp):
 
         # QUESTION 2 CODE: tokenize the webpage to get the number of words and then determine if it is the longest webpage
         word_tokens = []
+        
+        # create tokenizer that separates based on groups of whitespace
         reg_tokenizer = RegexpTokenizer('\s+', gaps=True)
-        kdsajkfas = re.compile(r'\W')
+
+        # create a regex object to match nonalphanumeric characters so we can remove them later on
+        alphanum_word = re.compile(r'\W')
+
+        # iterate over all words that tokenizer returns
         for word in reg_tokenizer.tokenize(text):
+            # if a word is not composed of only non-alphanumeric characters, or if it is greater than one character
+            # then we want to keep it
             if not re.match(r'^(\W+|^[\w+])$', word):
-                sanitized_word = kdsajkfas.sub("", word)
+                # remove all non-alphanumeric characters from the word
+                sanitized_word = alphanum_word.sub("", word)
+
+                # LOW INFO VALUE: if length of word is greater than 1, add to word_token list
+                #                 we do this to retain only relevant words
                 if len(sanitized_word) > 1: 
                     word_tokens.append(sanitized_word)
 
+        # LOW INFO VALUE: if # of words is less than 150, consider as a low info page
         if len(word_tokens) < 150:
             return []
-
+        
+        # if current page's word token count is greater than what we have seen, then make it the longest page
         word_token_count = len(word_tokens)        
         if word_token_count > longest_page[1]:
             longest_page = (url, word_token_count)        
         
         # QUESTION 3 CODE: get the 50 most common words across all the pages crawled
         for word in word_tokens:
+            # using a dictionary to count how many times the word appears in all pages
             if word.lower() not in stop_words:
                 word_count[word.lower()] += 1
             else:
+                # is a stop word and don't want to include in our tokens
                 word_tokens.remove(word)
 
         if len(word_tokens) < 150:
+            # LOW INFO VALUE: check again if, after removing any stop words, meets our threshold
             return []
 
+        # sort words highest to lowest based off their frequency and take top 50
         frequency = sorted(word_count.items(), key = lambda f: f[1], reverse = True)
         common_50 = [w[0] for w in frequency[:50]]
 
         output['QUESTION 3: 50 Most Common Words'] = common_50
 
         # QUESTION 4 CODE: do regex check to see if anything before ics in ics.uci.edu, retrieve it, add to dict along w defragmented url
+        # (https?:\/\/) : matches http with optional s and forward slash forward slash
+        # (\w*\.?ics\.uci\.edu)+ : matches ics.uci.edu with optional alphanumeric characters (subdomain) + optional '.' in front in case there is subdomain
+        # (.*) : matches anything or nothing afterwards in case there is a longer path
         parsed_url = re.search(r'(https?:\/\/)(\w*\.?ics\.uci\.edu)+(.*)', url_no_fragment)
         if parsed_url:
+            # we get the subdomain and add to dictionary with the full de-fragmented url
             subdomain = parsed_url.group(2)
             subdomains[subdomain].add(url_no_fragment)
 
@@ -145,22 +178,31 @@ def extract_next_links(url, resp):
     # lxml is the recommended page parser from assignment spec -> faster than html.parser
     page_soup = BeautifulSoup(resp.raw_response.content, "lxml")
     # for all links (identifiable by the <a> tag), get the link and add to frontier
+    # link.get('href') gets actual link from within the tag
     next_links = [link.get('href') for link in page_soup.find_all('a')]
     
     return next_links
 
 def is_valid(url) -> bool:
+    # if url is a none then its not valid
     if not url:
         return False
 
     try:
+        # if we've visited the sanitized url already then it is not valid
         url_no_fragment = sanitize_url(url)
         if url_no_fragment in unique_urls:
             return False
+
+        # make sure that the url has http(s) in the scheme
         parsed = urlparse(url)
         if parsed.scheme not in set(["http", "https"]):
             return False
-            
+        
+        # parsed.netloc is the domain of the url
+        # if the domain is within the list of valid urls
+        # and the url does not end with one certain paths we want to avoid
+        # and the url is not in a folder of the paths that we don't want to look at
         return re.search(r"(ics.uci.edu|cs.uci.edu|informatics.uci.edu|stat.uci.edu)", parsed.netloc.lower()) is not None and not re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico"
             + r"|png|tiff?|mid|mp2|mp3|mp4|Z|odc"
@@ -179,14 +221,14 @@ def is_valid(url) -> bool:
             + r"|thmx|mso|arff|rtf|jar|csv|ppsx|img"
             + r"|rm|smil|wmv|swf|wma|zip|rar|gz)\/", parsed.path.lower()) is None
 
-            
-
     except TypeError:
         print ("TypeError for ", parsed)
         raise
 
 def sanitize_url(url):
-    url_no_fragment = url.split('#')[0]
+    # cleans up the url, excluding unnecessary portions
+    url_no_fragment = url.split('#')[0] # we don't want the fragment -> leads to same page
+    # all the below queries lead to traps -> based off inspection
     url_no_fragment = url_no_fragment.split("/?replytocom=")[0]
     url_no_fragment = url_no_fragment.split("/?share=")[0]
     url_no_fragment = url_no_fragment.split("/?ical=")[0]
@@ -194,6 +236,8 @@ def sanitize_url(url):
     url_no_fragment = url_no_fragment.split("?action=")[0]
     url_no_fragment = url_no_fragment.split("?version=")[0]
     url_no_fragment = url_no_fragment.split("?afg")[0]
+    
+    # if the url is not none and the last char in the url string is a forwardslash -> remove since it gave duplicates
     if url_no_fragment and url_no_fragment[-1] == "/":
         url_no_fragment = url_no_fragment[:-1]
 
